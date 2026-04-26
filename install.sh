98ca74ab8f088b44dd4c5b1f5578641fcd8200e0
osvelaza <osvelaza@gmail.com>
2026-04-26 11:49:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://github.com/osvelaza/TFG-adminMV.git
-ruta=$pwd/TFG-adminMV
-sed -i -e "s|RUTAEDITAR|$ruta/GUI|g" $ruta/tfg.desktop
+ruta=$($pwd/TFG-adminMV)
+nueva=$($ruta/GUI)
+sed -i -e "s|RUTAEDITAR||g" $ruta/tfg.desktop
 mv $ruta/tfg.desktop ~/.local/share/applications/
 exit
